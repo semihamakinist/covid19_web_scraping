018128c03714c62567650b050bdfe7a71381fee5
Semiha MAKINIST <semihamakinist@gmail.com>
2021-09-08 16:34:09 +0300

    Create SaveDataCovid19.js

---
+++ SaveDataCovid19.js
@@ -0,0 +1,32 @@
+// https://covid19.saglik.gov.tr/TR-66935/genel-koronavirus-tablosu.html
+// geneldurumjson, json data file in javascript of "https://covid19.saglik.gov.tr/TR-66935/genel-koronavirus-tablosu.html"
+
+(function(console){
+ 
+console.save = function(data, filename){
+ 
+    if(!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+ 
+    if(!filename) filename = 'console.json'
+ 
+    if(typeof data === "object"){
+        data = JSON.stringify(data, undefined, 4)
+    }
+ 
+    var blob = new Blob([data], {type: 'text/json'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+ 
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+ }
+})(console)
+
+// save geneldurumjson data on console of chrome 
+console.save(geneldurumjson, "geneldurumjson.txt");
